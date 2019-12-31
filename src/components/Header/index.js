5833371bf773ced0846eaa0efb08ca7c50b5d7bb
oliveira-andre <desmonplays@gmail.com>
2019-12-30 21:26:11 -0400

    (new feature) initial commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React, { Fragment } from 'react';
+
+const Header = () => (
+  <Fragment>
+    <div className="Header">
+      <div className="container">
+        <div className="navbar">
+          <div className="navbar-brand">
+            <div className="navbar-burguer">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </Fragment>
+);
+
+export default Header;
